1b517dabbf5d23b69fa115c5e5a5667a5a00ba7d
Maciej Mensfeld <maciej@mensfeld.pl>
2022-08-01 09:21:36 +0200

    extract stats decorator from waterdrop

--- lib/karafka/core/version.rb
+++ lib/karafka/core/version.rb
@@ -4,6 +4,6 @@ module Karafka
   module Core
     # Current Karafka::Core version
     # We follow the versioning schema of given Karafka version
-    VERSION = '2.0.1'
+    VERSION = '2.0.2'
   end
 end
