c76eb8e1edf63cba03b134a1170e9642fb28e9aa
Maciej Mensfeld <maciej@mensfeld.pl>
2023-12-16 21:00:06 +0100

    explicit modules

---
+++ lib/karafka/core/helpers.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Karafka
+  module Core
+    # Namespace for some small utilities used across the ecosystem
+    module Helpers
+    end
+  end
+end
---
+++ lib/karafka/core/patches.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Karafka
+  module Core
+    # Patches to dependencies and components
+    module Patches
+    end
+  end
+end
