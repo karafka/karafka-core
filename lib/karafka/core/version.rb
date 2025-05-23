de198c7c2b075c0d7c4deffde64e52da3a8ee54a
Maciej Mensfeld <maciej@mensfeld.pl>
2025-05-23 16:16:36 +0200

    trusted publishing (#188)

--- lib/karafka/core/version.rb
+++ lib/karafka/core/version.rb
@@ -4,6 +4,6 @@ module Karafka
   module Core
     # Current Karafka::Core version
     # We follow the versioning schema of given Karafka version
-    VERSION = '2.5.0'
+    VERSION = '2.5.1'
   end
 end
