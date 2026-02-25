19d2ce25fd16c7fa4300f71e30440f2309668b94
Maciej Mensfeld <maciej@mensfeld.pl>
2026-02-25 11:05:52 +0100

    Remove Coditsu references
    Coditsu is no longer used for code quality checks.

--- spec/lib/karafka/core/configurable/node_spec.rb
+++ spec/lib/karafka/core/configurable/node_spec.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-# Nothing really here. Just a stub for Coditsu
-# This is tested in the `configurable_spec.rb` via the use-cases.
+# Tested in `configurable_spec.rb` via the use-cases.
