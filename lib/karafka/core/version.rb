8d73cc25577907573d4a9644304cd88dd14a7fac
Maciej Mensfeld <maciej@mensfeld.pl>
2026-06-10 18:04:27 +0200

    v2.6.0 (#369)

--- lib/karafka/core/version.rb
+++ lib/karafka/core/version.rb
@@ -4,6 +4,6 @@ module Karafka
   module Core
     # Current Karafka::Core version
     # We follow the versioning schema of given Karafka version
-    VERSION = "2.5.13"
+    VERSION = "2.6.0"
   end
 end
